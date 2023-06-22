2fde01de45facfa6c17706d0680ba8aa7ddb965c
@birdev2236 <birddev.sofwaredeveloping@gmail.com>
2023-06-21 22:39:13 -0400

    add endpoint to see videos

--- src/Pages/Anime.jsx
+++ src/Pages/Anime.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
+const api = "63077coow6x6m4zz2qwab";
+
+
+
 
 export  function Anime() {
-  return (
-    <div>
-        <h1>Ruta Anime</h1>
-    </div>
-  )
-}
+
+    const [videos, setVideos] = useState([]);
+    const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await axios.get(`https://api.streamsb.com/api/folder/list?key=${api}&fld_id=279289`);
+          setVideos(response.data.result.files);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+      fetchData();
+    }, []);
+  
+    const handleNextVideo = () => {
+      if (currentVideoIndex < videos.length - 1) {
+        setCurrentVideoIndex(currentVideoIndex + 1);
+      }
+    }
+  
+    const handlePreviousVideo = () => {
+      if (currentVideoIndex > 0) {
+        setCurrentVideoIndex(currentVideoIndex - 1);
+      }
+    }
+  
+    return (
+      <div>
+        {videos.length > 0 && (
+          <iframe
+            src={videos[currentVideoIndex].link}
+            width="600"
+            height="600"
+            frameBorder="0"
+            allowFullScreen
+          ></iframe>
+        )}
+        <button onClick={handlePreviousVideo}>Anterior</button>
+        <button onClick={handleNextVideo}>Siguiente</button>
+      </div>
+    );
+  }
+  
